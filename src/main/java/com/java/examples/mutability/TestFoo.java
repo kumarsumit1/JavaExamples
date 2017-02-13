4b6f780dc8633dfb2b9911de07c5f9dd5aad56db
kumarsumit1 <ursumit@gmail.com>
2017-02-14 02:57:56 +0530

    k

---
+++ src/main/java/com/java/examples/mutability/TestFoo.java
@@ -0,0 +1,14 @@
+package com.java.examples.mutability;
+
+public class TestFoo {
+
+	public static void main(String[] args) {
+		//Foo foo=new Foo("test");
+		//System.out.println(foo.getValue());
+		
+		
+		
+
+	}
+
+}
